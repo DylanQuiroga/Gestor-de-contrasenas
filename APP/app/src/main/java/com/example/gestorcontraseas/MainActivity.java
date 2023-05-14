db696a628a242882b07bf727357061da53d90eda
Dylan Quiroga Arancibia <dylan.quiroga@userena.cl>
2023-05-13 23:26:26 -0400

    Encriptamiento
    se ha creado una funcion que encripta y desencripta una cadena de texto para despues implementarse a la base de datos

--- APP/app/src/main/java/com/example/gestorcontraseas/MainActivity.java
+++ APP/app/src/main/java/com/example/gestorcontraseas/MainActivity.java
@@ -2,13 +2,88 @@ package com.example.gestorcontraseas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String LLAVE = "ASfnHF56RACTERFDQUIO1239";
+
+    private static final int ENCRYPT = 1;
+    private static final int DECRYPT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button boton = findViewById(R.id.boton);
+        MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String texto = "Hola cabrones jejeje";
+                String st1 = null;
+                String st2 = null;
+
+                try {
+                    st1 = CryptoUtils.encrypt(texto,LLAVE);
+                    Log.d("MainActivity", st1);
+                    st2 = CryptoUtils.decrypt(st1,LLAVE);
+                    Log.d("MainActivity", st2);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                /*values.put("tipoCuenta", 1);
+                values.put("sitio", "steam.com");
+                values.put("correo", "teamcompleto123@gmail.com");
+                values.put("contra", "123456789");
+                values.put("nombreUsuario", "DeadCell");
+                values.put("rut", "12.345.678-9");
+                values.put("celular","+56912345678");
+
+                db.insert("my_table",null,values);
+
+                List<String> records = dbHelper.getAllRecords();
+
+                for(String record : records){
+                    System.out.println(record);
+                }*/
+            }
+        });
     }
+
+    private void imprimirDB(List<String> records){
+        for(String record : records) System.out.println(record);
+    }
+
+    private void ingresarDatos(String tc, String st, String ce, String cn, String nu, String rut, String cel, String tf, String ns, String as, String cs, String dr, String o1, String o2, String o3, SQLiteDatabase DB){
+        ContentValues values = new ContentValues();
+        values.put("TipoCuenta", tc);
+        values.put("sitio", st);
+        values.put("correo", ce);
+        values.put("contra", cn);
+        values.put("nombreUsuario", nu);
+        values.put("rut", rut);
+        values.put("celular",cel);
+        values.put("telefonoFijo", tf);
+        values.put("nombres", ns);
+        values.put("apellidos", as);
+        values.put("correoSecundario", cs);
+        values.put("direccion", dr);
+        values.put("otro1",o1);
+        values.put("otro2",o2);
+        values.put("otro3",o3);
+        DB.insert("my_table", null, values);
+    }
+
 }
---
+++ APP/app/src/main/java/com/example/gestorcontraseas/MyDatabaseHelper.java
@@ -0,0 +1,65 @@
+package com.example.gestorcontraseas;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE my_table (id INTEGER PRIMARY KEY AUTOINCREMENT, tipoCuenta int, sitio TEXT, correo TEXT, contra TEXT, nombreUsuario TEXT, rut TEXT, celular TEXT, telefonoFijo TEXT, nombres TEXT, apellidos TEXT, correoSecundario TEXT, direccion TEXT, otro1 TEXT, otro2 TEXT, otro3 TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS mytable");
+        onCreate(db);
+    }
+
+    public List<String> getAllRecords() {
+        List<String> records = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM my_table", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range") String id = cursor.getString(cursor.getColumnIndex("id"));
+                @SuppressLint("Range") String tipoCuenta = cursor.getString(cursor.getColumnIndex("tipoCuenta"));
+                @SuppressLint("Range") String sitio = cursor.getString(cursor.getColumnIndex("sitio"));
+                @SuppressLint("Range") String correo = cursor.getString(cursor.getColumnIndex("correo"));
+                @SuppressLint("Range") String contra = cursor.getString(cursor.getColumnIndex("contra"));
+                @SuppressLint("Range") String nombreUsuario = cursor.getString(cursor.getColumnIndex("nombreUsuario"));
+                @SuppressLint("Range") String rut = cursor.getString(cursor.getColumnIndex("rut"));
+                @SuppressLint("Range") String celular = cursor.getString(cursor.getColumnIndex("celular"));
+                @SuppressLint("Range") String telefonoFijo = cursor.getString(cursor.getColumnIndex("telefonoFijo"));
+                @SuppressLint("Range") String nombres = cursor.getString(cursor.getColumnIndex("nombres"));
+                @SuppressLint("Range") String apellidos = cursor.getString(cursor.getColumnIndex("apellidos"));
+                @SuppressLint("Range") String correoSecundario = cursor.getString(cursor.getColumnIndex("correoSecundario"));
+                @SuppressLint("Range") String direccion = cursor.getString(cursor.getColumnIndex("direccion"));
+                @SuppressLint("Range") String otro1 = cursor.getString(cursor.getColumnIndex("otro1"));
+                @SuppressLint("Range") String otro2 = cursor.getString(cursor.getColumnIndex("otro2"));
+                @SuppressLint("Range") String otro3 = cursor.getString(cursor.getColumnIndex("otro3"));
+
+                String record = "ID: " + id + " | Tipo de cuenta: " + tipoCuenta + " | Sitio: " + sitio + " | Correo: " + correo + " | Contraseña: " + contra + " | Usuario: " + nombreUsuario + " | Rut: " + rut + " | Celular: " + celular + " | Teléfono fijo: " + telefonoFijo + " | Nombres: " + nombres + " | Apellidos: " + apellidos + " | Correo secundario: " + correoSecundario + " | Dirección: " + direccion + " | Otro 1: " + otro1 + " | Otro 2: " + otro2 + " | Otro 3: " + otro3;
+                records.add(record);
+            } while (cursor.moveToNext());
+        }
+
+        return records;
+    }
+
+}
