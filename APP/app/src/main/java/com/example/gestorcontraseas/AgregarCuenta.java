75a593e407b0ea3b3005e1ee9aab74f20f8e4e73
Dylan Quiroga Arancibia <dylan.quiroga@userena.cl>
2023-06-03 20:25:28 -0400

    .

---
+++ APP/app/src/main/java/com/example/gestorcontraseas/AgregarCuenta.java
@@ -0,0 +1,14 @@
+package com.example.gestorcontraseas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_cuenta);
+    }
+}
