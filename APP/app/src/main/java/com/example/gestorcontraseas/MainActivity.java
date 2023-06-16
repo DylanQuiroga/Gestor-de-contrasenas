6ecd3dc39c182fc2a159c486dab6e0600d876b68
Vicente Muñoz <vicente.munoz@userena.cl>
2023-06-16 19:02:41 -0400

    pruebas

--- APP/app/src/main/java/com/example/gestorcontraseas/MainActivity.java
+++ APP/app/src/main/java/com/example/gestorcontraseas/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
